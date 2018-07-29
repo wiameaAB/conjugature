12ee8d46ce6b1ca124efaee8c6c624b3e3915f87
wiameaAB <wiameabid@gmail.com>
2018-07-29 15:51:31 -0700

    écrire les termianisons

---
+++ conDATA.php
@@ -0,0 +1,9 @@
+$P_P=["je","tu","il","elle","nous","vous","ils","elles"];
+$Ter_P_1G=["e","es","e","ons","ez","ent"];
+§Ter_F_1G=["a","as","a","ons","ez","ont"];
+§Ter_P_C_1G=[é];
+
+
+§Ter_P_2G=["is","is","it","ssons","ssez","ssent"];
+§Ter_F_2G=["a","as","a","ons","ez","ont"];
+§Ter_P_C_2G=[i];
