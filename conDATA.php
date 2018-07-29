6ec1d4674a75beb492cc3a8b1580520482e9b373
wiameaAB <wiameabid@gmail.com>
2018-07-29 16:20:45 -0700

    corerection de §

--- conDATA.php
+++ conDATA.php
@@ -1,9 +1,7 @@
 $P_P=["je","tu","il","elle","nous","vous","ils","elles"];
 $Ter_P_1G=["e","es","e","ons","ez","ent"];
-§Ter_F_1G=["a","as","a","ons","ez","ont"];
-§Ter_P_C_1G=[é];
+$Ter_F_1G=["a","as","a","ons","ez","ont"];
 
 
-§Ter_P_2G=["is","is","it","ssons","ssez","ssent"];
-§Ter_F_2G=["a","as","a","ons","ez","ont"];
-§Ter_P_C_2G=[i];
+$Ter_P_2G=["is","is","it","ssons","ssez","ssent"];
+$Ter_F_2G=["a","as","a","ons","ez","ont"];
